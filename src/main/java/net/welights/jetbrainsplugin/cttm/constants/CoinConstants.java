b2a021fb97006c98a6d3cb5b46d711981583cfb7
freedom521fd@163.com <githubFreedom521@>
2021-02-27 13:33:42 +0800

    update title

--- src/main/java/net/welights/jetbrainsplugin/cttm/constants/CoinConstants.java
+++ src/main/java/net/welights/jetbrainsplugin/cttm/constants/CoinConstants.java
@@ -1,16 +1,16 @@
 package net.welights.jetbrainsplugin.cttm.constants;
 
 public class CoinConstants {
-    public static final String PLUGIN_NAME = "暴富 To Be A Rich Man";
+    public static final String PLUGIN_NAME = "To Be A Rich Man";
     //非小号api暂时不支持筛选功能
     public static final String COIN_INFO_URL = "https://fxhapi.feixiaohao.com/public/v1/ticker?convert=CNY&limit=";
-    public static final String COIN_NAME = "名称";
-    public static final String SYMBOL = "代码";
-    public static final String COIN_LATEST_PRICE_USD = "当前价格(美元)";
-    public static final String COIN_LATEST_PRICE_CNY = "当前价格(人民币)";
-    public static final String RISE_AND_FALL_RATIO_1Hour = "涨跌幅(1小时)";
-    public static final String RISE_AND_FALL_RATIO_24Hour = "涨跌幅(24小时)";
-    public static final String RISE_AND_FALL_RATIO_7Day = "涨跌幅(7天)";
+    public static final String COIN_NAME = "Name";
+    public static final String SYMBOL = "Code";
+    public static final String COIN_LATEST_PRICE_USD = "U.S. dollar";
+    public static final String COIN_LATEST_PRICE_CNY = "CNY";
+    public static final String RISE_AND_FALL_RATIO_1Hour = "Ratio(1Hour)";
+    public static final String RISE_AND_FALL_RATIO_24Hour = "Ratio(24Hour)";
+    public static final String RISE_AND_FALL_RATIO_7Day = "Ratio(7 Day)";
     public static final String TIMESTAMP_FORMATTER = "yyyy-MM-dd HH:mm:ss";
     public static final String REFRESH_TIMESTAMP = "Updated at: %s";
 
